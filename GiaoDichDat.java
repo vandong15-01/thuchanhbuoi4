566943aa0e5a133ea32139d285b7d3f33ad69e82
vandong15-01 <125889405+vandong15-01@users.noreply.github.com>
2023-05-22 15:52:26 +0700

    GiaoDichDat.java

---
+++ GiaoDichDat.java
@@ -0,0 +1,26 @@
+package QuanLyGiaoDichNhaDat;
+
+public class GiaoDichDat extends GiaoDich {
+    private String loaiDat;
+public GiaoDichDat(){
+    super();
+}
+public GiaoDichDat(String loaiDat){
+    this.loaiDat = loaiDat;
+}
+public String getLoaiDat(){
+    return loaiDat;
+}
+public void setLoaiDat(String loaiDat){
+    this.loaiDat = loaiDat;
+}
+public void nhap(){
+    super.nhap();
+    System.out.print("Nhap loai dat (A/B/C): ");
+    loaiDat = scanner.nextLine();
+}
+@Override
+public String toString(){
+    return super.toString() + ",loai dat: " + this.loaiDat;
+}
+}
